761722c1f604c0f39fa6f7c516e0edd25255499d
Doris Vertegaal <doris.vertegaal@deltares.nl>
2024-12-04 15:30:38 +0100

    created file self

--- scripts/create_file.py
+++ scripts/create_file.py
@@ -2,17 +2,16 @@
 
 ######################################################################################
 ####################### Content of the python script #################################
-from pathlib import Path
+from pathlib import Path 
 
-# Main function
+# Main function 
 def create_file(ouputfile: Path, lines: list) -> None:
     """
     Create a file with each line contant stored as items in ``lines`` list. Saves it to ``outputfile``.
     """
-    with open(outputfile, "w") as file:
+    with open(outputfile, 'w') as file:
         for line in lines:
             file.write(f"{line}\n")
-
 
 # Get the snakemake arguments
 outputfile = Path(str(snakemake.output))
@@ -20,4 +19,4 @@ line1 = snakemake.params.line1
 line2 = snakemake.params.line2
 
 # Run our function
-create_file(outputfile, lines=[line1, line2])
+create_file(outputfile, lines = [line1, line2])
